e2cf7240f4b06f6b569449f1d52a521d6c281c57
Vladimir Iglovikov <ternaus@users.noreply.github.com>
2019-04-25 07:22:38 -0700

    Added support for pytorch_segmentation_models (#3)

--- augmentations.py
+++ augmentations.py
@@ -8,11 +8,31 @@ def get_train_transform():
     return albu.Compose([
         albu.PadIfNeeded(min_height=crop_height, min_width=crop_width, p=1),
         albu.RandomSizedCrop((int(0.3 * crop_height), 288), crop_height, crop_width, p=1),
-        albu.ShiftScaleRotate(rotate_limit=45, p=1),
-        albu.HueSaturationValue(p=0.5),
-        albu.RandomGamma(p=1),
-        albu.RandomBrightnessContrast(p=1, brightness_limit=0.5, contrast_limit=0.5),
         albu.HorizontalFlip(p=0.5),
+        albu.OneOf([
+            albu.IAAAdditiveGaussianNoise(p=0.5),
+            albu.GaussNoise(p=0.5),
+        ], p=0.2),
+        albu.OneOf([
+            albu.MotionBlur(p=0.2),
+            albu.MedianBlur(blur_limit=3, p=0.1),
+            albu.Blur(blur_limit=3, p=0.1),
+        ], p=0.2),
+        albu.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0, rotate_limit=20, p=0.1),
+        albu.OneOf([
+            albu.OpticalDistortion(p=0.3),
+            albu.GridDistortion(p=0.1),
+            albu.IAAPiecewiseAffine(p=0.3),
+        ], p=0.2),
+        albu.OneOf([
+            albu.CLAHE(clip_limit=2, p=0.5),
+            albu.IAASharpen(p=0.5),
+            albu.IAAEmboss(p=0.5),
+            albu.RandomBrightnessContrast(p=0.5),
+        ], p=0.3),
+        albu.HueSaturationValue(p=0.3),
+        albu.JpegCompression(p=0.2, quality_lower=20, quality_upper=99),
+        albu.ElasticTransform(p=0.1),
         albu.Normalize(p=1)
     ], p=1)
 
